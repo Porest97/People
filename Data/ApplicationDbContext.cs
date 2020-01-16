01e5261280c29bc2fa42331e808f027574d8f425
Per Orest <per.orest@outlook.com>
2020-01-16 09:45:28 +0100

    Add project files.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using People.Models.DataModels;
+
+namespace People.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<People.Models.DataModels.Person> Person { get; set; }
+    }
+}
---
+++ Models/ViewModels/PeopleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using People.Models.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace People.Models.ViewModels
+{
+    public class PeopleViewModel
+    {      
+
+        public List<Person> People { get; internal set; }
+    }
+}
